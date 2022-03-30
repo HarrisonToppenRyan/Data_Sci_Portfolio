e7f77984252f573a95e33b569480e73e86317e21
Harrison Toppen-Ryan <harrison619@live.com>
2022-03-29 22:22:57 -0700

    Update lists_script.py

--- Medical_Insurance_Project/lists_script.py
+++ Medical_Insurance_Project/lists_script.py
@@ -14,7 +14,6 @@ rohan_insurance_cost = estimate_insurance_cost(name =
 # Estimate Valentina's insurance cost
 valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
 
-# Add your code here
 names = ["Maria", "Rohan", "Valentina"]
 insurance_costs = [4150.0, 5320.0, 35210.0]
 insurance_data = list(zip(names, insurance_costs))
@@ -33,7 +32,6 @@ print("Estimated cost: " + str(estimated_insurance_data))
 names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
 insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
 
-# Add your code here
 names.append("Priscilla")
 insurance_costs.append(8320.0)
 medical_records = list(zip(names, insurance_costs))
